32fd32ce9546c40042e37ae423179f883f0c07e9
Mohamed Abdelwahed <mho2002@ymail.com>
2025-03-11 21:04:18 +0200

    final modifications

--- src/main/java/com/example/service/MainService.java
+++ src/main/java/com/example/service/MainService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 
+import com.example.repository.MainRepository;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public abstract class MainService<T> {
 
-    
+    private final MainRepository<T> mainRepository;
+
+    public MainService(MainRepository<T> mainRepository) {
+        this.mainRepository = mainRepository;
+    }
+
+    protected MainRepository<T> getMainRepository() {
+        return this.mainRepository;
+    }
 
     
     
