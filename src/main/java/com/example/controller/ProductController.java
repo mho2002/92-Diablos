537e6e96819e5ef511e1074862a5ee36057d94d8
Mohamedhishamm <162007882+Mohamedhishamm@users.noreply.github.com>
2025-03-11 20:21:02 +0200

    oackage com.example.service;
    import com.example.model.Product;
    import com.example.repository.ProductRepository;
    import com.example.repository.UserRepository;
    import org.springframework.stereotype.Service;
    
    import java.util.ArrayList;
    import java.util.UUID;
    
    @Service
    @SuppressWarnings("rawtypes")
    public class ProductService extends MainService<Product> {
    
        public ProductService(ProductRepository productRepository) {
        super(productRepository);
        }
    
        private final ProductRepository productRepository= (ProductRepository) this.getMainRepository();
    
        public Product addProduct(Product product)
        {
            return productRepository.addProduct(product);
        }
    
        public ArrayList<Product> getProducts()
        {
            return productRepository.getProducts();
        }
    
        public Product getProductById(UUID productId)
        {
            return productRepository.getProductById(productId);
        }
    
        public Product updateProduct(UUID productId, String newName, double newPrice)
        {
            return productRepository.updateProduct(productId, newName, newPrice);
        }
    
        public void applyDiscount(double discount, ArrayList<UUID> productIds)
        {
            productRepository.applyDiscount(discount, productIds);
        }
    
        public void deleteProductById(UUID productId)
        {
            productRepository.deleteProductById(productId);
        }
    
    }
    Updated product services , product and product controller

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import com.example.model.Product;
+import com.example.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,46 +11,68 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/product")
+//allTested
 public class ProductController {
-//The Dependency Injection Variables
-//The Constructor with the requried variables mapping the Dependency Injection.
 
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {this.productService = productService;}
+    //tested
     @PostMapping("/")
     public Product addProduct(@RequestBody Product product)
     {
+        if (product.getId() == null)
+            product.setId(UUID.randomUUID());
+        if (product.getName() == null || product.getName().isEmpty())
+            throw new IllegalArgumentException("Product name cannot be empty or null");
+        else if (product.getPrice() <= 0) {
+            throw new IllegalArgumentException("Product price cannot be less than or equal 0");
+        }
 
+        return productService.addProduct(product);
     }
-
+    //tested
     @GetMapping("/")
     public ArrayList<Product> getProducts()
     {
-
+        return productService.getProducts();
     }
-
+    //tested
     @GetMapping("/{productId}")
     public Product getProductById(@PathVariable UUID productId)
     {
-
+        return productService.getProductById(productId);
     }
-
+    //tested
     @PutMapping("/update/{productId}")
     public Product updateProduct(@PathVariable UUID productId, @RequestBody Map<String,Object> body)
     {
-
+        return productService.updateProduct( productId, (String) body.get("newName"),
+                Double.parseDouble(body.get("newPrice").toString()));
     }
-
+    //tested
     @PutMapping("/applyDiscount")
     public String applyDiscount(@RequestParam double discount,@RequestBody ArrayList<UUID> productIds)
     {
-
+        try {
+            productService.applyDiscount(discount / 100, productIds);
+            return "Discount applied successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
-
+    //tested
     @DeleteMapping("/delete/{productId}")
     public String deleteProductById(@PathVariable UUID productId)
     {
-
+        try {
+            productService.deleteProductById(productId);
+            return "success delete product ";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
 
 }
-
